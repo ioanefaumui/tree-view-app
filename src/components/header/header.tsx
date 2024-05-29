3d9aebacd642584dff13049334cbffd59be1ac6c
ioaneSuflex <ioane@suflex.com.br>
2024-05-29 20:21:50 -0300

    feat: header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,32 @@
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo-tractian.png";
+import gold from "../../assets/icons/gold.png";
+import "./header.css";
+import { useCompanies } from "../../hooks/use-companies";
+
 export function Header() {
-  return <header>This is the header</header>;
+  const { companies } = useCompanies();
+
+  return (
+    <header>
+      <Link to={"/"}>
+        <img src={logo} alt="Tractian logo" />
+      </Link>
+
+      {companies.length > 0 && (
+        <nav>
+          <ul>
+            {companies.map((c) => (
+              <li key={c.id}>
+                <Link to={"/"}>
+                  <img src={gold} alt="Asset icon" />
+                  <span>{c.name} Unit</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
 }
